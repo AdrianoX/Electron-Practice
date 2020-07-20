51d5bb7a5a82bc8d2d0fae11305971658d8f2edd
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-07-20 19:20:11 +0200

    Init Electron Project

---
+++ app/script.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { render } from "react-dom";
+
+const AppDescription = () => (
+  <div>
+    <p>
+      According to optometrists in order to save your eyes, you should follow
+      the 20/20/20. It means you should to rest your eyes every 20 minutes for
+      20 seconds by looking more than 20 feet away.
+    </p>
+    <p>
+      This app will help you track your time and inform you when it's time to
+      rest.
+    </p>
+  </div>
+);
+
+class App extends React.Component {
+  state = {
+    status: "off",
+    time: null,
+    timer: null,
+  };
+
+  formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time - minutes * 60);
+
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+  };
+
+  step = () => {};
+
+  startTimer = () => {
+    this.setState({
+      timer: setInterval(this.step, 1000),
+    });
+  };
+
+  render() {
+    const { status } = this.state;
+
+    return (
+      <div>
+        <h1>Protect your eyes</h1>
+        {status === "off" && <AppDescription />}
+        {status === "work" && <img src="./images/work.png" />}
+        {status === "rest" && <img src="./images/rest.png" />}
+        {status !== "off" && <div className="timer">18:23</div>}
+        {status === "off" && <button className="btn">Start</button>}
+        {status !== "off" && <button className="btn">Stop</button>}
+        <button className="btn btn-close">X</button>
+      </div>
+    );
+  }
+}
+render(<App />, document.querySelector("#app"));
